d7051d540984fe684611ddd64554e1e35a05347f
ahtarv <atharvkpatil@gmail.com>
2026-02-16 12:18:21 +0530

    Training code

---
+++ Untitled-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Problem_A_Round_1051{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t= sc.nextInt();
+        for(int i=0 ; i<t ;i++)
+        {
+            
+            int x = sc.nextInt();
+            n
+            for(i=0;i<x;i++)
+            
+        }
+    }
+}
